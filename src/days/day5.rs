b2064a06d318717bfd658d68a5c22f14e69f018c
jitsem <jitse.meulenijzer@gmail.com>
2025-12-06 12:45:01 +0100

    Day 5 - part 2 actual reasonable time

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -1,6 +1,5 @@
 use crate::common::day::Day;
 use crate::common::util::{self};
-use std::collections::{BTreeSet, HashSet};
 use std::error::Error;
 use std::ops::RangeInclusive;
 
@@ -37,13 +36,61 @@ impl Day for Day5 {
         Ok(fresh_count.to_string())
     }
 
+    //357907198933892
     fn solve_part2(&self, input: &str) -> Result<String, Box<dyn Error>> {
         let Some((range, _)) = input.split_once("\n\n") else {
             return Err("Expected correct input format".into());
         };
-        let set: HashSet<usize> =
-            util::Util::extract_all_numbers_from_str_range(range, "\n").collect();
-        Ok(set.iter().count().to_string())
+        dbg!(range.lines().count());
+        let all_ranges: Vec<RangeInclusive<usize>> =
+            util::Util::extract_all_ranges_from_str_range(range, "\n").collect();
+        let nr_of_range_items: usize = Self::range_iter_to_distint(all_ranges.iter())
+            .iter_mut()
+            .map(|r| r.end() - r.start() + 1)
+            .sum();
+        Ok(nr_of_range_items.to_string())
+    }
+}
+
+impl Day5 {
+    fn range_iter_to_distint<'a>(
+        ranges: impl Iterator<Item = &'a RangeInclusive<usize>>,
+    ) -> Vec<RangeInclusive<usize>> {
+        let mut distinct: Vec<RangeInclusive<usize>> = Vec::new();
+        let mut set_changed = false;
+        for range in ranges {
+            let mut skip_add = false;
+            for d_range in distinct.iter_mut() {
+                if d_range.contains(range.start()) && d_range.contains(range.end()) {
+                    skip_add = true;
+                    break;
+                }
+                if range.contains(d_range.start()) && range.contains(d_range.end()) {
+                    *d_range = *range.start()..=*range.end();
+                    skip_add = true;
+                    break;
+                }
+                if d_range.contains(range.start()) {
+                    *d_range = *d_range.start()..=*range.end();
+                    skip_add = true;
+                    break;
+                }
+                if d_range.contains(range.end()) {
+                    *d_range = *range.start()..=*d_range.end();
+                    skip_add = true;
+                    break;
+                }
+            }
+            if !skip_add {
+                distinct.push(range.clone());
+            } else {
+                set_changed = true;
+            }
+        }
+        if set_changed {
+            distinct = Self::range_iter_to_distint(distinct.iter());
+        }
+        distinct
     }
 }
 
